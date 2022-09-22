e7a2d18a3f1ba226764755fae8fdae7fadffe84b
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-22 14:32:21 -0500

    Finished the exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, string year, string price, string recommendation, string isdrivable, string honknoise, string enginenoise)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            Recommendation = recommendation;
+            IsDrivable = isdrivable;
+            HonkNoise = honknoise;
+            EngineNoise = enginenoise;
+        }
+
+        public string Make { get; set; }
+        
+
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Price { get; set; }
+        public string Recommendation { get; set; }
+        public string Isdrivable { get; }
+        public string IsDrivable { get; set; }
+        public string Honknoise { get; }
+        public string HonkNoise { get; set; }
+        public string Enginenoise { get; }
+
+        public void makeHonkNoise(string noise)
+        {
+            Console.WriteLine(noise);
+        }
+        public string EngineNoise { get; set; }
+        
+        public void makeEngineNoise(string noise)
+        {
+            Console.WriteLine(noise);
+        }
+
+        public void PrintStuff()
+        {
+            Console.WriteLine($"Brand: {Make}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Recommendation status: {Recommendation}");
+            Console.WriteLine($"Is the car drivable: {IsDrivable}");
+            Console.WriteLine($"Honk noise: {HonkNoise}");
+            Console.WriteLine($"Engine noise: {EngineNoise}");
+        }
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,15 +13,77 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            //DONE ^
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+
+            //Porsche 911 Targa 4 GTS 2023 $162,700
+            //2023 Chevrolet Corvette Z06 has a starting price of $106,395
+            //Mclaren, the speedtail starting price 2,100,000 est. 2018
+
+
+            var car1 = new Car();
+
+            car1.Make = "Porsche";
+            car1.Model = "911 Targa 4 GTS";
+            car1.Year = "2023";
+            car1.Price = "$162,700";
+            car1.Recommendation = "Yes";
+            car1.IsDrivable = "Yes";
+            car1.HonkNoise = "Normal";
+            car1.EngineNoise = "Loud";
+
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Start");
+            Console.WriteLine("------------------------------------------------------------");
+
+            car1.PrintStuff();
+
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("NEXT CAR");
+            Console.WriteLine("------------------------------------------------------------");
+
+
+            var car2 = new Car();
+            {
+                car2.Make = "Chevrolet Corvette";
+                car2.Model = "Z06";
+                car2.Year = "2023";
+                car2.Price = "$106,395";
+                car2.Recommendation = "Yes";
+                car2.IsDrivable = "Yes";
+                car2.HonkNoise = "Normal";
+                car2.EngineNoise = "Loud";
+
+                car2.PrintStuff();
+
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("NEXT CAR");
+                Console.WriteLine("------------------------------------------------------------");
+
+            }
+
+           
+
+            var car3 = new Car( "Mclaren", "The Speedtail", "2018", "$2,250,000 est.", "No, unless your filthy rich! lol :)", "Yes", "Normal", "Loud");
+
+            car3.PrintStuff();
+
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Finish 'ps: now your a little bit smarter :)'  ");
+            Console.WriteLine("------------------------------------------------------------");
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            // #1
 
+            // ABOVE
+            
             //*************BONUS X 2*************//
 
             //Create a CarLot class
